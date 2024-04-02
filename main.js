6c30f667282ae44cdaeab4be344fc1656408f615
REZI-1995 <rekapanadze@gmail.com>
2024-04-02 16:11:28 +0400

    finish

--- main.js
+++ main.js
@@ -1,1 +1,10 @@
-console.log('hello world')
+
+let myName = "My name is, rezi";
+let iAm = "i am"
+let wasBorn = "old i was born"
+let yearBd = "1995.years"
+const yearOfBirth = 1995;
+const currentYear = 2024;
+let myHobby = "and my hobby is traveling";
+let myAge = currentYear - yearOfBirth;
+console.log(myName,iAm,myAge,wasBorn,yearBd,myHobby);
